2f00ab59ca5093c238dec8aa0cdce323a22929ea
abyal <alex.aby555@gmail.com>
2023-05-22 15:10:51 +0100

    3rd

--- onemore.tf
+++ onemore.tf
@@ -1,9 +1,14 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "web"
-  }
+provider "aws" {
+  region = "us-east-1"  # Specify your desired region
 }
 
+resource "aws_instance" "example" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"  # Specify the AMI ID of the desired Amazon Machine Image (AMI)
+  instance_type = "t2.micro"  # Specify the desired instance type
+
+  key_name      = "test"  # Specify the name of the key pair to associate with the instance
+
+  tags = {
+    Name = "example-instance"
+  }
+}
