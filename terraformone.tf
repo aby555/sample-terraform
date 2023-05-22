78018f1af06a3724c6caa4a0990d80ec3a3f07b4
abyal <alex.aby555@gmail.com>
2023-05-22 14:08:45 +0100

    3rd

--- terraformone.tf
+++ terraformone.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
 resource "aws_instance" "web" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -13,16 +25,4 @@ resource "aws_instance" "test" {
   tags = {
     Name = "HelloWorld"
   }
-}
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.67.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
 }
