da34b89977d366483780bebf725283c61e04fafb
abyal <alex.aby555@gmail.com>
2023-05-22 15:04:49 +0100

    3rd

--- onemore.tf
+++ onemore.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "web"
   }
 }
 resource "aws_instance" "test" {
@@ -11,6 +11,6 @@ resource "aws_instance" "test" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "test"
   }
 }
