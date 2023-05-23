0811228c610db21af6e1a7854ec5a6a41aa9b52d
abyal <alex.aby555@gmail.com>
2023-05-23 14:10:41 +0100

    3rd

--- terraformone.tf
+++ terraformone.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+output "frontend" {
+  value = aws_instance.frontend.public_ip
+}
+
+
 resource "aws_instance" "mysql" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
